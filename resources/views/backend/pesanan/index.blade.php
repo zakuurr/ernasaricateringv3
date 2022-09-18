b27b4c84fd280c10702de88738f150178b9368e7
albeefillah <albeefillah17@gmail.com>
2022-09-18 17:18:03 +0700

    tambah nama

--- resources/views/backend/pesanan/index.blade.php
+++ resources/views/backend/pesanan/index.blade.php
@@ -13,75 +13,49 @@
       </div>
       <!-- /.card-header -->
       <div class="card-body">
-        {{-- <a href="{{ route('pesanan.create') }}" class="btn btn-success" style="margin-bottom:10px;">+ Tambah Menu</a> --}}
+        <a href="{{ route('user.create') }}" class="btn btn-success" style="margin-bottom:10px;">+ Tambah User</a>
         <table id="example1" class="table table-bordered table-striped">
           <thead>
           <tr>
             <th>No</th>
+            <th>Nama</th>
             <th>Order ID</th>
             <th>Sub total</th>
             <th>Total</th>
-            {{-- <th>Nama Lengkap</th>
-            <th>Email</th>
-            <th>No Hp</th>
-            <th>Alamat</th> --}}
             <th>Status</th>
             <th>Order Date</th>
-            <th colspan="2" class="text-center">Action</th>
+            <th>Action</th>
           </tr>
           </thead>
           <tbody>
             @foreach ($pesananOr as $key => $item)
             <tr>
-              <td>{{ $key+1 }}</td>
-              <td>{{ $item->id }}</td>
-              <td>{{ $item->subtotal }}</td>
-              <td>{{ $item->total }}</td>
-              {{-- <td>{{ $item->nama_lengkap }}</td>
-              <td>{{ $item->email }}</td>
-              <td>{{ $item->nohp }}</td>
-              <td>{{ $item->alamat }}</td> --}}
-              <td>{{ $item->status }}</td>
-              <td>{{ $item->created_at }}</td>
-              {{-- <td>
-                @if($item->status == 1)
-                <span class="badge badge-danger"> Belum Bayar </span>
-                     @elseif ($item->status == 2)
-                     <span class="badge badge-success"> Sudah Bayar </span>
-                     @elseif ($item->status == 3)
-                     <span class="badge badge-warning"> Di Batalkan </span>
-                     @elseif ($item->status == 4)
-                     <span class="badge badge-primary"> Di Proses </span>
-                     @elseif ($item->status == 5)
-                     <span class="badge badge-info"> Di Antar </span>
-                     @elseif ($item->status == 6)
-                     <span class="badge badge-success"> Selesai </span>
-                     @endif
+                <td>{{ $key+1 }}</td>
+                <td>{{ $item->user->name }}</td>
+                <td>{{ $item->id }}</td>
+                <td>{{ $item->subtotal }}</td>
+                <td>{{ $item->total }}</td>
+                <td>{{ $item->status }}</td>
+                <td>{{ $item->created_at }}</td>
+                <td>
+                    <a style="margin-bottom : 5px;" class="btn btn-warning" href="{{ route('pesanan.detail', $item->id) }}" ><font color="white"><i class="fa fa-eye"></i> Lihat detail</font></a>
+                  <div class="dropdown">
+                      <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Status
+                      </button>
+                      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="{{ route('pesanan.updateOrderStatus',['id'=>$item->id,'status'=>'diproses'])}}">Di Proses</a>
+                        <a class="dropdown-item" href="{{ route('pesanan.updateOrderStatus',['id'=>$item->id,'status'=>'sedang-dikirim'])}}">Sedang Dikirim</a>
+                        {{-- <a class="dropdown-item" href="{{ route('pesanan.updateOrderStatus',['id'=>$item->id,'status'=>'pesanan-diterima'])}}">Sedang Dikirim</a> --}}
+                        <a class="dropdown-item" href="{{ route('pesanan.updateOrderStatus',['id'=>$item->id,'status'=>'cancel'])}}">Cancel</a>
 
-
-              </td> --}}
-              <td>
-                <center>
-                  <a class="btn btn-warning" href="{{ route('pesanan.detail', $item->id) }}" ><font color="white"><i class="fa fa-eye"></i> Lihat detail</font></a>
-                  {{-- <a class="btn btn-danger tombol-hapus" href="{{ route('pesanan.destroy', $item->id) }}"><i class="fas fa-trash" ></i>Hapus</a> --}}
-                </center>
-              </td>
-              <td>
-                <div class="dropdown">
-                    <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                     Status
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                      <a class="dropdown-item" href="{{ route('pesanan.updateOrderStatus',['id'=>$item->id,'status'=>'dikirim'])}}">Dikirim</a>
-                      <a class="dropdown-item" href="{{ route('pesanan.updateOrderStatus',['id'=>$item->id,'status'=>'cancel'])}}">Cancel</a>
-
+                      </div>
                     </div>
-                  </div>
 
-            </td>
-            </div>
+              </td>
+              </div>
             </tr>
-            @endforeach
+    @endforeach
           </tbody>
 
         </table>
@@ -89,7 +63,36 @@
       <!-- /.card-body -->
     </div>
     <!-- /.card -->
+    {{-- @foreach ($pesananOr as $key => $item)
+    <tr>
+      <td>{{ $key+1 }}</td>
+      <td>{{ $item->user->name }}</td>
+      <td>{{ $item->id }}</td>
+      <td>{{ $item->subtotal }}</td>
+      <td>{{ $item->total }}</td>
+      <td>{{ $item->status }}</td>
+      <td>{{ $item->created_at }}</td>
+      <td>
+        <center>
+          <a class="btn btn-warning" href="{{ route('pesanan.detail', $item->id) }}" ><font color="white"><i class="fa fa-eye"></i> Lihat detail</font></a>
+        </center>
+      </td>
+      <td>
+        <div class="dropdown">
+            <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+             Status
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="{{ route('pesanan.updateOrderStatus',['id'=>$item->id,'status'=>'dikirim'])}}">Dikirim</a>
+              <a class="dropdown-item" href="{{ route('pesanan.updateOrderStatus',['id'=>$item->id,'status'=>'cancel'])}}">Cancel</a>
 
+            </div>
+          </div>
+
+    </td>
+    </div>
+    </tr>
+    @endforeach --}}
     @section('js')
 <script>
 
@@ -120,6 +123,23 @@ if (result.value) {
 
 });
 
+</script>
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      // "buttons": ["excel", "pdf", "print"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
 </script>
 @endsection
 @endsection
